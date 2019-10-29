d9dd4a78b6e42fd29bdfaad23758ff97ec725bea
Nathalia <natylafosse@gmail.com>
2019-10-29 19:14:57 -0400

    20 mins

---
+++ difference.py
@@ -0,0 +1,14 @@
+
+
+def diff(array):
+     for number in array:  
+        comp = 0
+        if number > comp:
+            the_highest_calue = number
+            comp = number
+        print (the_highest_calue)
+
+
+if __name__ == "__main__":
+    the_array = [15, 22, 84, 14, 88, 23]
+    diff(the_array)
